6df5b558356410c1fa2319fd9f631552d98c4e45
Mahamud <mhbidhan@outlook.com>
2023-04-13 00:33:08 +0600

    Defined Login page and styles for it

---
+++ src/pages/Login/Login.page.jsx
@@ -0,0 +1,64 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { loginCaterer } from '../../services/caterer';
+import CustomButton from './../../components/common/CustomButton/CustomButton.component';
+import InputField from './../../components/common/InputField/InputField.component';
+import './Login.styles.scss';
+
+const Login = () => {
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = async (e) => {
+    try {
+      e.preventDefault();
+
+      const { email, password } = formData;
+
+      const a = await loginCaterer(email, password);
+      console.log(a);
+      document.location = '/';
+    } catch (error) {
+      console.log(error.response.data);
+    }
+  };
+
+  return (
+    <div className="login">
+      <form className="login-form" onSubmit={handleSubmit}>
+        <InputField
+          label={'Email'}
+          name="email"
+          value={formData.email}
+          handleChange={handleChange}
+        />
+        <InputField
+          label="Password"
+          name="password"
+          value={formData.password}
+          type="password"
+          handleChange={handleChange}
+        />
+
+        <CustomButton type="submit" theme={'contained'} label={'Submit'} />
+
+        <div className="signup-link">
+          Don't have an account{' '}
+          <Link to="/signup" underline="hover">
+            Signup
+          </Link>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
