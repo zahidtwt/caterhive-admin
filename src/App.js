f34294ff78907540b7bb3543aa8187568454a8d8
Mahamud <mhbidhan@outlook.com>
2023-04-13 00:52:42 +0600

    Added user functionality to the app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import { useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import './App.css';
 import Authenticated from './Routes/Authenticated/Authenticated.component';
-import Unauthenticated from './Routes/Authenticated/Unauthenticated/Unauthenticated.component';
+import Unauthenticated from './Routes/Unauthenticated/Unauthenticated.component';
+import { getOwnData } from './services/caterer';
 
 function App() {
   const [user, setUser] = useState(null);
+
+  const getUser = useCallback(async () => {
+    const user = await getOwnData();
+
+    setUser(user);
+  }, []);
+
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+
+    if (token) getUser();
+  }, [getUser]);
 
   return (
     <div className="App">{user ? <Authenticated /> : <Unauthenticated />}</div>
