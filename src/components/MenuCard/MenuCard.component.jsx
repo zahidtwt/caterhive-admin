2f7633296d9d4b06590f30c8f1d21ee921d7ce9b
Mahamud <mhbidhan@outlook.com>
2023-04-16 16:16:54 +0600

    Defiend MenuCard component

---
+++ src/components/MenuCard/MenuCard.component.jsx
@@ -0,0 +1,25 @@
+import { Card } from '@mui/material';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import React from 'react';
+
+const MenuCard = ({ menu }) => {
+  const { title, thumbnail, description } = menu;
+
+  return (
+    <Card sx={{ width: 250, height: 400 }}>
+      <CardMedia sx={{ height: 220 }} image={thumbnail} title={title} />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          {title}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          {description}
+        </Typography>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default MenuCard;
