d0438c8d73f041dfec881b18638484f97ff50e35
Mahamud <mhbidhan@outlook.com>
2023-04-16 16:23:05 +0600

    Defined services for food-items and menu API

---
+++ src/services/foodItems.js
@@ -0,0 +1,27 @@
+import http from './http';
+
+export async function getAllFoodItems({ search = '', searchBy = 'title' }) {
+  try {
+    const query = search ? `?searchBy=${searchBy}search=${search}` : '';
+    const { data } = await http.get(
+      `${process.env.REACT_APP_API_ENDPOINT}/foodItems${query}`
+    );
+
+    return data;
+  } catch (error) {
+    throw error;
+  }
+}
+
+export async function createNewFoodItem(newFoodItem) {
+  try {
+    const { data } = await http.post(
+      `${process.env.REACT_APP_API_ENDPOINT}/foodItems`,
+      newFoodItem
+    );
+
+    return data;
+  } catch (error) {
+    throw error;
+  }
+}
---
+++ src/services/menu.js
@@ -0,0 +1,27 @@
+import http from './http';
+
+export async function getAllMenus({ search = '', searchBy = 'title' }) {
+  try {
+    const query = search ? `?searchBy=${searchBy}search=${search}` : '';
+    const { data } = await http.get(
+      `${process.env.REACT_APP_API_ENDPOINT}/menus${query}`
+    );
+
+    return data;
+  } catch (error) {
+    throw error;
+  }
+}
+
+export async function createNewMenu(newMenu) {
+  try {
+    const { data } = await http.post(
+      `${process.env.REACT_APP_API_ENDPOINT}/menus`,
+      newMenu
+    );
+
+    return data;
+  } catch (error) {
+    throw error;
+  }
+}
