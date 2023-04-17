b7a7b10e923d51df1de17d96c44d2e768f0e8867
Your Name <you@example.com>
2023-04-17 19:31:59 +0600

    Defined dayMenu service with the GET and POST methods

---
+++ src/services/dayMenu.js
@@ -0,0 +1,27 @@
+import http from './http';
+
+export async function getAllDayMenus({ search = '', searchBy = 'title' }) {
+  try {
+    const query = search ? `?searchBy=${searchBy}search=${search}` : '';
+    const { data } = await http.get(
+      `${process.env.REACT_APP_API_ENDPOINT}/dayMenus${query}`
+    );
+
+    return data;
+  } catch (error) {
+    throw error;
+  }
+}
+
+export async function createNewDayMenu(newDayMenu) {
+  try {
+    const { data } = await http.post(
+      `${process.env.REACT_APP_API_ENDPOINT}/dayMenus`,
+      newDayMenu
+    );
+
+    return data;
+  } catch (error) {
+    throw error;
+  }
+}
