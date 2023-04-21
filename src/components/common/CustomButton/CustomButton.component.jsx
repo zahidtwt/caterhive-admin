8e7a3d3949ac0d5bd48c6b13d2e4d8729f2e6195
Mahamud <mhbidhan@outlook.com>
2023-04-21 16:02:36 +0600

    Modified CustomButton component to add a label to it

--- src/components/common/CustomButton/CustomButton.component.jsx
+++ src/components/common/CustomButton/CustomButton.component.jsx
@@ -2,15 +2,20 @@ import { Button } from '@mui/material';
 import React from 'react';
 import './CustomButton.styles.scss';
 
-const CustomButton = ({ label, theme, handleClick, ...otherProps }) => {
+const CustomButton = ({
+  label = 'Submit',
+  theme,
+  handleClick,
+  ...otherProps
+}) => {
   return (
     <Button
       variant={theme}
-      sx={{ padding: '13px' }}
+      sx={{ padding: '13px', color: '#fff' }}
       onClick={handleClick}
       {...otherProps}
     >
-      Submit
+      {label}
     </Button>
   );
 };
