59d57b5750b69964ab33b6b5166fb133966fcb3c
Mahamud <mhbidhan@outlook.com>
2023-04-23 17:45:30 +0600

    Modified FoodItemCard to view price and category data

--- src/components/FoodItemCard/FoodItemCard.component.jsx
+++ src/components/FoodItemCard/FoodItemCard.component.jsx
@@ -5,15 +5,41 @@ import Typography from '@mui/material/Typography';
 import * as React from 'react';
 
 const FoodItemCard = ({ foodItem }) => {
-  const { title, imgUrl, description } = foodItem;
+  const { title, imgUrl, description, price, category } = foodItem;
   return (
     <Card sx={{ width: 250, height: 400 }}>
       <CardMedia sx={{ height: 220 }} image={imgUrl} title={title} />
       <CardContent>
+        <Typography
+          variant="h6"
+          sx={{
+            marginBottom: '1rem',
+          }}
+        >
+          {price} BDT
+        </Typography>
         <Typography gutterBottom variant="h5" component="div">
           {title}
         </Typography>
-        <Typography variant="body2" color="text.secondary">
+        <Typography
+          variant="body2"
+          sx={{
+            marginBottom: '1rem',
+          }}
+          component="div"
+        >
+          {category.name}
+        </Typography>
+        <Typography
+          variant="body2"
+          color="text.secondary"
+          sx={{
+            whiteSpace: 'nowrap',
+            width: '100%',
+            overflow: 'hidden',
+            textOverflow: 'ellipsis',
+          }}
+        >
           {description}
         </Typography>
       </CardContent>
