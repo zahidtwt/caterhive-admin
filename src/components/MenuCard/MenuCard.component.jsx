3bede48d36ef4112088408b19a3fdebbcd0f988a
Mahamud <mhbidhan@outlook.com>
2023-04-23 19:36:00 +0600

    Modified the MenuCard to view the price of the menu

--- src/components/MenuCard/MenuCard.component.jsx
+++ src/components/MenuCard/MenuCard.component.jsx
@@ -5,16 +5,33 @@ import Typography from '@mui/material/Typography';
 import React from 'react';
 
 const MenuCard = ({ menu }) => {
-  const { title, thumbnail, description } = menu;
+  const { title, thumbnail, price, description } = menu;
 
   return (
     <Card sx={{ width: 250, height: 400, margin: '1rem' }}>
       <CardMedia sx={{ height: 220 }} image={thumbnail} title={title} />
       <CardContent>
+        <Typography
+          variant="h6"
+          sx={{
+            marginBottom: '1rem',
+          }}
+        >
+          {price} BDT
+        </Typography>
         <Typography gutterBottom variant="h5" component="div">
           {title}
         </Typography>
-        <Typography variant="body2" color="text.secondary">
+        <Typography
+          variant="body2"
+          color="text.secondary"
+          sx={{
+            whiteSpace: 'nowrap',
+            width: '100%',
+            overflow: 'hidden',
+            textOverflow: 'ellipsis',
+          }}
+        >
           {description}
         </Typography>
       </CardContent>
