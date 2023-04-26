ce4e0055be639c7e7f126a7c201efff722956ad8
Mahamud <mhbidhan@outlook.com>
2023-04-26 12:12:57 +0600

    Defined EventMenuForm and it's validator

---
+++ src/components/EventMenuForm/EventMenuForm.component.jsx
@@ -0,0 +1,230 @@
+import { Autocomplete, TextField } from '@mui/material';
+import React, { useCallback, useEffect, useState } from 'react';
+import { getOwnData } from '../../services/caterer';
+import { createNewEventMenu } from '../../services/eventMenus';
+import { getAllFoodItemsByCategory } from '../../services/foodItems';
+import CustomButton from '../common/CustomButton/CustomButton.component';
+import InputField from '../common/InputField/InputField.component';
+import eventMenuValidatorSchema from './eventMenuForm.validator';
+
+const EventMenuForm = ({ setModal, setEventMenus }) => {
+  const [formData, setFormData] = useState({
+    title: '',
+    appetizers: [],
+    mainCourses: [],
+    desserts: [],
+    drinks: [],
+  });
+  const [foodItemOptions, setFoodItemOptions] = useState({
+    caterer: '',
+    appetizers: [],
+    mainCourses: [],
+    desserts: [],
+    drinks: [],
+  });
+  const [foodItemSearch, setFoodItemSearch] = useState(null);
+
+  const [serverError, setServerError] = useState(null);
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  const updateFoodItems = (data) => {
+    setErrorMessage(null);
+    setServerError(null);
+    setFormData({ ...formData, ...data });
+  };
+
+  const handleSubmit = async (e) => {
+    try {
+      e.preventDefault();
+
+      const { error } = eventMenuValidatorSchema.validate(formData);
+
+      if (error) {
+        setErrorMessage({
+          [error?.details[0]?.path[0]]: error?.message,
+        });
+        return;
+      }
+
+      const newEventMenu = await createNewEventMenu(formData);
+
+      setEventMenus((eventMenus) => [...eventMenus, newEventMenu]);
+      setModal(false);
+    } catch (error) {
+      setServerError(error.response.data);
+    }
+  };
+
+  const getFoodItems = useCallback(async () => {
+    const user = await getOwnData();
+
+    setFormData((formData) => ({ ...formData, caterer: user._id }));
+
+    const appetizers = await getAllFoodItemsByCategory({
+      search: foodItemSearch,
+      caterer: user._id,
+      category: 'Appetizer',
+    });
+    const mainCourses = await getAllFoodItemsByCategory({
+      search: foodItemSearch,
+      caterer: user._id,
+      category: 'Main Course',
+    });
+    const desserts = await getAllFoodItemsByCategory({
+      search: foodItemSearch,
+      caterer: user._id,
+      category: 'Desert',
+    });
+    const drinks = await getAllFoodItemsByCategory({
+      search: foodItemSearch,
+      caterer: user._id,
+      category: 'Drinks',
+    });
+
+    setFoodItemOptions({
+      appetizers,
+      mainCourses,
+      desserts,
+      drinks,
+    });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  useEffect(() => {
+    getFoodItems();
+  }, [getFoodItems, foodItemSearch]);
+  return (
+    <div className="custom">
+      <form className="form" onSubmit={handleSubmit}>
+        <InputField
+          label={'title'}
+          name="title"
+          value={formData.title}
+          handleChange={(e) => {
+            setErrorMessage(null);
+            setServerError(null);
+            setFormData({ ...formData, title: e.target.value });
+          }}
+          error={errorMessage?.title}
+        />
+
+        <Autocomplete
+          multiple
+          sx={{
+            marginBottom: '30px',
+          }}
+          options={foodItemOptions.appetizers}
+          onChange={(e, newValue) => {
+            e.preventDefault();
+            setErrorMessage(null);
+            setServerError(null);
+            updateFoodItems({
+              appetizers: newValue.map((foodItem) => foodItem._id),
+            });
+          }}
+          filterSelectedOptions
+          getOptionLabel={(option) => option.title || option}
+          renderInput={(params) => (
+            <TextField
+              {...params}
+              onChange={(e) =>
+                setFoodItemSearch({ appetizers: e.target.value })
+              }
+              variant="outlined"
+              label="Appetizers"
+              error={errorMessage?.appetizers}
+            />
+          )}
+        />
+
+        <Autocomplete
+          multiple
+          sx={{
+            marginBottom: '30px',
+          }}
+          options={foodItemOptions.mainCourses}
+          onChange={(e, newValue) => {
+            e.preventDefault();
+            setErrorMessage(null);
+            setServerError(null);
+            updateFoodItems({
+              mainCourses: newValue.map((foodItem) => foodItem._id),
+            });
+          }}
+          filterSelectedOptions
+          getOptionLabel={(option) => option.title || option}
+          renderInput={(params) => (
+            <TextField
+              {...params}
+              onChange={(e) =>
+                setFoodItemSearch({ mainCourses: e.target.value })
+              }
+              variant="outlined"
+              label="Main Courses"
+              error={errorMessage?.mainCourses}
+            />
+          )}
+        />
+
+        <Autocomplete
+          multiple
+          sx={{
+            marginBottom: '30px',
+          }}
+          options={foodItemOptions.desserts}
+          onChange={(e, newValue) => {
+            e.preventDefault();
+            setErrorMessage(null);
+            setServerError(null);
+            updateFoodItems({
+              desserts: newValue.map((foodItem) => foodItem._id),
+            });
+          }}
+          filterSelectedOptions
+          getOptionLabel={(option) => option.title || option}
+          renderInput={(params) => (
+            <TextField
+              {...params}
+              onChange={(e) => setFoodItemSearch({ desserts: e.target.value })}
+              variant="outlined"
+              label="Desserts"
+              error={errorMessage?.desserts}
+            />
+          )}
+        />
+
+        <Autocomplete
+          multiple
+          sx={{
+            marginBottom: '30px',
+          }}
+          options={foodItemOptions?.drinks}
+          onChange={(e, newValue) => {
+            e.preventDefault();
+            setErrorMessage(null);
+            setServerError(null);
+            updateFoodItems({
+              drinks: newValue.map((foodItem) => foodItem._id),
+            });
+          }}
+          filterSelectedOptions
+          getOptionLabel={(option) => option.title || option}
+          renderInput={(params) => (
+            <TextField
+              {...params}
+              onChange={(e) => setFoodItemSearch({ drinks: e.target.value })}
+              variant="outlined"
+              label="Drinks"
+              error={errorMessage?.drinks}
+            />
+          )}
+        />
+
+        {serverError ? <p className="error-message">{serverError}</p> : null}
+        <CustomButton type="submit" theme={'contained'} label={'Submit'} />
+      </form>
+    </div>
+  );
+};
+
+export default EventMenuForm;
---
+++ src/components/EventMenuForm/eventMenuForm.validator.js
@@ -0,0 +1,32 @@
+import joi from 'joi';
+
+const eventMenuValidatorSchema = joi.object({
+  caterer: joi.string().min(24).max(24).required().label('Caterer'),
+  title: joi.string().min(3).max(255).required().label('Title'),
+  appetizers: joi
+    .array()
+    .min(1)
+    .items(joi.string().min(24).max(24))
+    .required()
+    .label('Appetizers'),
+  mainCourses: joi
+    .array()
+    .min(1)
+    .items(joi.string().min(24).max(24))
+    .required()
+    .label('Main Courses'),
+  desserts: joi
+    .array()
+    .min(1)
+    .items(joi.string().min(24).max(24))
+    .required()
+    .label('Desserts'),
+  drinks: joi
+    .array()
+    .min(1)
+    .items(joi.string().min(24).max(24))
+    .required()
+    .label('Drinks'),
+});
+
+export default eventMenuValidatorSchema;
