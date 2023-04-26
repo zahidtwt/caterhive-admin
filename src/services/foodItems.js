150117e73ddbbb570bacec3e0a6bdd69ad299d9c
Mahamud <mhbidhan@outlook.com>
2023-04-26 12:07:57 +0600

    Defined a new service methods to get food items by category

--- src/services/foodItems.js
+++ src/services/foodItems.js
@@ -13,6 +13,24 @@ export async function getAllFoodItems({ search = '', searchBy = 'title' }) {
   }
 }
 
+export async function getAllFoodItemsByCategory({
+  search = '',
+  searchBy = 'title',
+  category,
+  caterer,
+}) {
+  try {
+    const query = search ? `&searchBy=${searchBy}search=${search}` : '';
+    const { data } = await http.get(
+      `${process.env.REACT_APP_API_ENDPOINT}/foodItems/category?caterer=${caterer}&categoryName=${category}${query}`
+    );
+
+    return data;
+  } catch (error) {
+    throw error;
+  }
+}
+
 export async function createNewFoodItem(newFoodItem) {
   try {
     const { data } = await http.post(
