b64f8dffdc9fa75ec48eefd80a002daa8c069036
mubtasim <mubtasim91@gmail.com>
2023-05-01 19:06:29 +0600

    attempt to run unit test. having problem with esm module system

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,9 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+// import http from "./services/http";
+// import {loginCaterer} =
 
-test('renders learn react link', () => {
+test("renders learn react link", () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
