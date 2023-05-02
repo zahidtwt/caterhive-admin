f629224c35aa6b9388e6b3d56106b082c3764d56
mubtasim <mubtasim91@gmail.com>
2023-05-02 09:42:48 +0600

    added Add New Menu Item to E2E tesing

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -9,21 +9,37 @@ describe("template spec", () => {
     cy.get('input[type="password"]').type("qaqaqaqa");
     cy.contains("Submit").click();
 
-    // Add Food Item
     cy.get('button[aria-label="open drawer"]').click();
-    cy.contains("food items").click();
-    cy.contains("Add new Food Item").click();
-    cy.get('input[name="title"]').type("Alu Vorta");
-    cy.get('input[name="description"]').type(
-      "Kaca Moric ar sorisar teler spicy & tasty alu vorta"
-    );
-    cy.get('input[name="price"]').type("50");
-    cy.get('input[role="combobox"]').click();
-    cy.get('div[role="presentation"]').contains("Main Course").click();
-    cy.contains("Select an Image").selectFile("aluvorta.jpg");
-    cy.get('button[type="submit"]').click();
+
+    // Add Food Item
+    // cy.contains("food items").click();
+    // cy.contains("Add new Food Item").click();
+    // cy.get('input[name="title"]').type("Alu Vorta");
+    // cy.get('input[name="description"]').type(
+    //   "Kaca Moric ar sorisar teler spicy & tasty alu vorta"
+    // );
+    // cy.get('input[name="price"]').type("50");
+    // cy.get('input[role="combobox"]').click();
+    // cy.get('div[role="presentation"]').contains("Main Course").click();
+    // cy.contains("Select an Image").selectFile("aluvorta.jpg");
+    // cy.get('button[type="submit"]').click();
 
     // Add Menu
+    cy.contains("menus").click();
+    cy.contains("Add new menu").click();
+    cy.get('input[name="title"]').type("Osthir dish");
+    cy.get('input[name="description"]').type("Khailei shanti");
+    cy.get('input[name="tags"]').type("Biriyani, Chap, Coffee");
+    cy.contains("Select an Thumbnail").selectFile("osthirDish.jpg");
+
+    const foodItems = ["Biriyani", "Chap", "Coffee"];
+
+    foodItems.forEach((item) => {
+      cy.get('input[role="combobox"]').click();
+      cy.get('div[role="presentation"]').contains(item).click();
+    });
+
+    cy.get('button[type="submit"]').click();
 
     // Add Day Menu
 
